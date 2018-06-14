607b69acb806829b9381e493b5cb0cbab3bc5f49
vercy <megavercy@gmail.com>
2018-06-15 05:46:25 +0900

    Addtl test coverage

--- navichart/src/test/groovy/com/cloverleaf/navigator/chart/InsetsTest.groovy
+++ navichart/src/test/groovy/com/cloverleaf/navigator/chart/InsetsTest.groovy
@@ -12,6 +12,11 @@ class InsetsTest extends Specification {
         expect:
         i1.hashCode() == i2.hashCode() && i1 == i2
 
+        i1.left == 0
+        i1.right== 0
+        i1.top == 0
+        i1.bottom == 0
+
         i1.toString() == "{left=0, top=0, bottom=0, right=0}"
     }
 }
