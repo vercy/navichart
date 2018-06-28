ab59a26f1db2895078c724ab8dd06d6493179824
vercy <megavercy@gmail.com>
2018-06-29 04:21:23 +0900

    Adding more charts to the main frame

--- navichart-viewer/src/main/java/com/cloverleaf/navigator/navichartviewer/ViewerFrame.java
+++ navichart-viewer/src/main/java/com/cloverleaf/navigator/navichartviewer/ViewerFrame.java
@@ -1,23 +1,20 @@
 package com.cloverleaf.navigator.navichartviewer;
 
 import javax.swing.*;
+import java.awt.*;
 
 class ViewerFrame extends JFrame {
-
-    private JNaviChart chart;
 
     void initUI() {
         setTitle("NaviChart");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        chart = new JNaviChart();
-        getContentPane().add(chart);
+        getContentPane().setLayout(new GridLayout(2,2));
+        getContentPane().add(new JNaviChart());
+        getContentPane().add(new JNaviChart());
+        getContentPane().add(new JNaviChart());
+        getContentPane().add(new JNaviChart());
         setSize(1024, 768);
         setLocationRelativeTo(null); // center on screen
     }
 
-    // TODO add event for updating frame title to match filename
-
-    JNaviChart getChart() {
-        return chart;
-    }
 }
