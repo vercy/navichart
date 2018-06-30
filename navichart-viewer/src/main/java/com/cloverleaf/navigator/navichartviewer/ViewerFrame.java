92f2f1c236a85d6f2b51cb226dea4c4704d7d121
vercy <megavercy@gmail.com>
2018-06-30 22:11:29 +0900

    Better birds eye view for initial test grid

--- navichart-viewer/src/main/java/com/cloverleaf/navigator/navichartviewer/ViewerFrame.java
+++ navichart-viewer/src/main/java/com/cloverleaf/navigator/navichartviewer/ViewerFrame.java
@@ -5,14 +5,40 @@ import java.awt.*;
 
 class ViewerFrame extends JFrame {
 
+    private static final double ONE_SIXTH = 1.0 / 6;
+
     void initUI() {
         setTitle("NaviChart");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        getContentPane().setLayout(new GridLayout(2,2));
-        getContentPane().add(new JNaviChart());
-        getContentPane().add(new JNaviChart());
-        getContentPane().add(new JNaviChart());
-        getContentPane().add(new JNaviChart());
+        getContentPane().setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.BOTH;
+        
+        c.weightx = ONE_SIXTH;
+        c.weighty = ONE_SIXTH;
+        c.gridx = 0;
+        c.gridy = 0;
+        getContentPane().add(new JNaviChart(), c);
+
+        c.weightx = ONE_SIXTH * 5;
+        c.weighty = ONE_SIXTH;
+        c.gridx = 1;
+        c.gridy = 0;
+        getContentPane().add(new JNaviChart(), c);
+
+        c.weightx = ONE_SIXTH;
+        c.weighty = ONE_SIXTH * 5;
+        c.gridx = 0;
+        c.gridy = 1;
+        getContentPane().add(new JNaviChart(), c);
+
+        c.weightx = ONE_SIXTH * 5;
+        c.weighty = ONE_SIXTH * 5;
+        c.gridx = 1;
+        c.gridy = 1;
+        getContentPane().add(new JNaviChart(), c);
+
+
         setSize(1024, 768);
         setLocationRelativeTo(null); // center on screen
     }
